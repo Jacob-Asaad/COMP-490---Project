c306e6b8ce6c180be9714700f19df0950857d5ca
valladaresassc <valladares.jose805@gmail.com>
2022-10-12 16:33:05 -0700

    test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text> hello jacob</Text>
+      <Text> hello jose</Text>
       <StatusBar style="auto" />
     </View>
   );
