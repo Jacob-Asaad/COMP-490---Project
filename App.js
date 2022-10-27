6255aa2c420d25fd901f6705eaf0c7516816c06f
valladaresassc <valladares.jose805@gmail.com>
2022-10-27 00:13:25 -0700

    Welcome screen test

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import welcomeScreen from './screens/welcomeScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text> hello jose</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <welcomeScreen />
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
