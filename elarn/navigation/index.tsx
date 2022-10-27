ad86358a4eac4b000c2f922fbdec3d50e7f6944c
valladaresassc <valladares.jose805@gmail.com>
2022-10-26 23:16:00 -0700

    Revert "Yarn test"
    This reverts commit 329c51ec1820df7106d25232cc99db3668e7a5f4.

--- elarn/navigation/index.tsx
+++ elarn/navigation/index.tsx
@@ -62,41 +62,35 @@ function BottomTabNavigator() {
     <BottomTab.Navigator
       initialRouteName="TabOne"
       screenOptions={{
+
         tabBarActiveTintColor: Colors[colorScheme].tint,
+        
       }}>
-      <BottomTab.Screen
+
+       <BottomTab.Screen
         name="TabOne"
         component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Modal')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
-        })}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-home" color={color} />,
+        }}
       />
       <BottomTab.Screen
         name="TabTwo"
         component={TabTwoScreen}
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-folder-outline" color={color} />,
         }}
       />
+      <BottomTab.Screen
+        name="TabThree"
+        component={TabThreeScreen}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-chatbox-outline" color={color} />,
+        }}
+      />
+     
+    
     </BottomTab.Navigator>
-  );
-}
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
@@ -106,4 +100,4 @@ function TabBarIcon(props: {
   color: string;
 }) {
   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
-}
+}}
