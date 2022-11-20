de1f2b599d6964fc174f6ca28e7402079cda8fbc
Steven Fein <saf0929@gmail.com>
2022-11-19 20:14:04 -0800

    updated .gitignore

--- config.js
+++ config.js
@@ -3,3 +3,13 @@ import {API_KEY} from "@env"
 export default{
     API_KEY,
 }
+
+// To use these variables in other locations, 
+// make sure to
+// import config from "./config"
+//
+// Have a file in the root of the project called .env and
+// initialize the variables you want, example below
+// API_KEY=abc123
+//
+//access the variable by {config.API_KEY}
