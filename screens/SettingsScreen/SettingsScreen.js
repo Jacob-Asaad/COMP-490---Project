418ba436b49c227336a65ae70d3ab2aed0cb2e40
mveraway <margarita.vera.984@my.csun.edu>
2022-12-10 22:47:38 -0800

    cleaning up/formatting code

--- screens/SettingsScreen/SettingsScreen.js
+++ screens/SettingsScreen/SettingsScreen.js
@@ -1,10 +1,7 @@
 import { View, SafeAreaView, Text, Image, Switch, StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
-import React, {useState, useEffect} from 'react';
-import Logo from '../../assets/images/logo.png';
-import CustomInput from '../../components/CustomInput/CustomInput';
+import React, { useState, useEffect } from 'react';
 import CustomButton from '../../components/CustomButton/Custombutton';
 import CustomSwitch from '../../components/CustomSwitch/CustomSwitch';
-import { Header } from 'react-native/Libraries/NewAppScreen';
 import { firebase } from '../../config';
 
 
@@ -23,154 +20,154 @@ const SettingsScreen = () => {
     })
   }, [])
 
-    const [notifications, SetNotifications] = useState(false);
-    const [bluetooth, SetBluetooth] = useState(false);
-    const [autowater, SetAutowater] = useState(false);
+  const [notifications, SetNotifications] = useState(false);
+  const [bluetooth, SetBluetooth] = useState(false);
+  const [autowater, SetAutowater] = useState(false);
 
-    const toggleBluetooth = () => SetBluetooth(previousState => !previousState);
-    const toggleAutowater = () => SetAutowater(previousState => !previousState);
-    const toggleNotifications= () => SetNotifications(previousState => !previousState);
+  const toggleBluetooth = () => SetBluetooth(previousState => !previousState);
+  const toggleAutowater = () => SetAutowater(previousState => !previousState);
+  const toggleNotifications = () => SetNotifications(previousState => !previousState);
 
-    const clearHistory = () => {
-        console.warn("Clearing History...");
-    }
+  const clearHistory = () => {
+    console.warn("Clearing History...");
+  }
 
-    const editProfile = () => {
-        console.warn("Editing Profile...");
-    }
+  const editProfile = () => {
+    console.warn("Editing Profile...");
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-        <View style={styles.contain}>
+      <View style={styles.contain}>
         <View style={styles.profileImage}>
-            <Image source={require('../../assets/images/profilepic.jpeg')} style={styles.image}>
+          <Image source={require('../../assets/images/profilepic.jpeg')} style={styles.image}>
 
-            </Image>
+          </Image>
         </View>
 
-        <Text style = {styles.emailName}>
-        {email.email}
-         </Text>
-          
-         <Text style = {styles.settings}> 
-         Settings
-         </Text>
+        <Text style={styles.emailName}>
+          {email.email}
+        </Text>
 
-         <View style = {styles.rows}>
-        <Text style = {styles.text}> 
-         Notifications 
-         </Text>
+        <Text style={styles.settings}>
+          Settings
+        </Text>
 
-        <CustomSwitch style = {styles.switch} 
-         isEnabled = {notifications} 
-         toggleSwitch = {SetNotifications}
-        />
-        
+        <View style={styles.rows}>
+          <Text style={styles.text}>
+            Notifications
+          </Text>
+
+          <CustomSwitch style={styles.switch}
+            isEnabled={notifications}
+            toggleSwitch={SetNotifications}
+          />
+
         </View>
 
-        <View style = {styles.rows}>
-        <Text style = {styles.text}> 
-         Bluetooth          
-         </Text>
-        
-        <CustomSwitch style = {styles.switch}
-         isEnabled = {bluetooth} 
-         toggleSwitch = {SetBluetooth}
-         />
+        <View style={styles.rows}>
+          <Text style={styles.text}>
+            Bluetooth
+          </Text>
+
+          <CustomSwitch style={styles.switch}
+            isEnabled={bluetooth}
+            toggleSwitch={SetBluetooth}
+          />
         </View>
 
-        <View style = {styles.rows}>
+        <View style={styles.rows}>
 
-        <Text style = {styles.text}> 
-         Automatic Watering
-         </Text>
-    
-        <CustomSwitch style = {styles.switch}
-         isEnabled = {autowater} 
-         toggleSwitch = {SetAutowater}
-         />
+          <Text style={styles.text}>
+            Automatic Watering
+          </Text>
 
-       </View>
-        <CustomButton style = {styles.button}
-        text = "Clear History" 
-        onPress={clearHistory}
-        bgColor = "#5A5A5A"
-        fgColor= "#000000" 
-        />
+          <CustomSwitch style={styles.switch}
+            isEnabled={autowater}
+            toggleSwitch={SetAutowater}
+          />
 
-        <CustomButton 
-        text = "Edit Profile " 
-        onPress={editProfile}
-        bgColor = "#5A5A5A"
-        fgColor= "#000000" 
+        </View>
+        <CustomButton style={styles.button}
+          text="Clear History"
+          onPress={clearHistory}
+          bgColor="#5A5A5A"
+          fgColor="#000000"
         />
 
+        <CustomButton
+          text="Edit Profile "
+          onPress={editProfile}
+          bgColor="#5A5A5A"
+          fgColor="#000000"
+        />
 
-        </View>
+
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#FFFFFF',
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
 
-    },
-    contain: {
-        flex: 1,
-        alignContent: 'center',
-        margin: 25,
-        padding: 20,
-        
-    },
-    rows : {
-      flexDirection: 'row',
-      paddingTop: 10,
-      paddingBottom: 2,
-      justifyContent: 'space-between',
-    },
+  },
+  contain: {
+    flex: 1,
+    alignContent: 'center',
+    margin: 25,
+    padding: 20,
 
-    image:{
-       flex: 1,
-       width: undefined,
-       height: undefined,
-       paddingTop: 10,
-    },
-    profileImage:{
-      alignSelf: 'center',
-        width: 200,
-        height: 200,
-        borderRadius: 100,
-        overflow: "hidden",
-        marginTop: 50,
-},
-    text: {
-        fontSize: 15,
-        fontWeight: 'bold',
-      paddingTop: 20,
-      marginRight: 145,
-      marginBottom: 5,
-    },
+  },
+  rows: {
+    flexDirection: 'row',
+    paddingTop: 10,
+    paddingBottom: 2,
+    justifyContent: 'space-between',
+  },
 
-    emailName: {
-      alignItems: "center",
-      alignSelf: "center",
-      marginTop: 20,
-      fontSize: 15,
+  image: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
+    paddingTop: 10,
+  },
+  profileImage: {
+    alignSelf: 'center',
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    overflow: "hidden",
+    marginTop: 50,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    paddingTop: 20,
+    marginRight: 145,
+    marginBottom: 5,
+  },
+
+  emailName: {
+    alignItems: "center",
+    alignSelf: "center",
+    marginTop: 20,
+    fontSize: 15,
   },
   settings: {
     alignItems: "center",
-      alignSelf: "center",
-      marginTop: 20,
-      marginBottom: 10,
-      fontSize: 25,
-      fontWeight: "bold",
+    alignSelf: "center",
+    marginTop: 20,
+    marginBottom: 10,
+    fontSize: 25,
+    fontWeight: "bold",
   },
   switch: {
     flex: 1,
     alignSelf: "right",
-    
+
   },
   buttons: {
     align: "center",
@@ -178,6 +175,6 @@ const styles = StyleSheet.create({
 
 
 });
-      
+
 
 export default SettingsScreen
