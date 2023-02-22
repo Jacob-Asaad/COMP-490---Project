eaf2adb823875f6b2d55e55736ae9849750eee66
valladaresassc <valladares.jose805@gmail.com>
2023-02-22 14:40:53 -0800

    Started working on Plant Profile
    - Added Plus Button on Plant Profile Screen
    - Created plantProfileForm
    - Trying to figure out how to get the form to pop up when the plus button is pressed

--- components/Styles/Styling.js
+++ components/Styles/Styling.js
@@ -61,11 +61,12 @@ export const customInputStyles = {
               },
               buttons: {
                 margin: 20,
-               alignSelf: 'center',
+                alignSelf: 'flex-end',
                 backgroundColor: "#577157",
                 borderRadius: 100,
                 height: 60,
                 width: 60,
+                
               }
           };
 
@@ -159,6 +160,7 @@ export const customInputStyles = {
           container: {
             flex: 1,
             backgroundColor: '#FFFFFF',
+            alignContent: "space-around",
           },
           text: {
               fontSize: 15,
@@ -168,13 +170,13 @@ export const customInputStyles = {
             marginBottom: 5,
           },
         errorText: {
+          alignContent: "space-around",
+          top: -60,
           alignItems: "center",
             alignSelf: "center",
-            marginTop: 20,
-            marginBottom: 10,
             fontSize: 25,
             fontWeight: "bold",
-            padding: 125,
+           
         },
       
       };
