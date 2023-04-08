8b16cf139a15b1718055881b04ee5321fd17d6b3
valladaresassc <valladares.jose805@gmail.com>
2023-04-08 03:36:55 -0700

    Worked on setting up Cloud functions to send notifications

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,27 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const { Expo } = require('expo-server-sdk');
 
 admin.initializeApp();
-const expo = new Expo();
+const expo = new Expo({ accessToken: 'oxq6VuA_yrYliTDLeN0oCnwmyCammz09ct3ZG0TR' });
 
 exports.sendNotificationOnDatabaseUpdate = functions.database
   .ref('/moistureSensor/')
   .onUpdate(async (change, context) => {
-    // Get the updated data from the Realtime Database
     const data = change.after.val();
 
     // Get the push notification token for the user with the updated data
     const userId = data.userId;
-    const snapshot = await admin.database().ref(`users/${userId}/pushToken`).once('value');
-    const pushToken = snapshot.val();
+    const userSnapshot = await admin.database().ref(`users/${userId}`).once('value');
+    const user = userSnapshot.val();
+    const pushToken = user.expoPushToken;
 
     // Construct the Expo push notification message
     const message = {
       to: pushToken,
       sound: 'default',
-      title: 'Plantlink',
+      title: 'PlantLink',
       body: 'Plant Data Updated',
-     // data: { data },
     };
 
     // Send the Expo push notification
@@ -34,16 +33,50 @@ exports.sendNotificationOnDatabaseUpdate = functions.database
     }
   });
 
+  exports.sendDailyNotification = functions.pubsub.schedule('every 24 hours').onRun(async (context) => {
+    const usersSnapshot = await db.collection('users').get();
+  
+    // Iterate through each user
+    usersSnapshot.forEach(async (doc) => {
+      const uid = doc.id;
+      const userData = doc.data();
+  
+      // Check if user has given permission to receive notifications
+      if (userData.receiveNotifications) {
+  
+        // Check the last notification timestamp
+        const lastNotificationTimestamp = userData.lastNotificationTimestamp || 0;
+const currentTime = Date.now();
 
+if ((currentTime - lastNotificationTimestamp) >= (24 * 60 * 60 * 1000)) {
+  // It's been more than 24 hours since last notification
 
+  // Get the push notification token for the user
+  const pushTokenSnapshot = await db.collection('users').doc(uid).collection('pushTokens').doc('expoPushToken').get();
+  const pushToken = pushTokenSnapshot.data().token;
 
+  // Construct the Expo push notification message
+  const message = {
+    to: pushToken,
+    sound: 'default',
+    title: 'PlantLink',
+    body: "It's time to water your plants!",
+  };
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  // Send the Expo push notification
+  try {
+    const response = await expo.sendPushNotificationsAsync([message]);
+    console.log(response);
 
+    // Update the user's last notification timestamp
+    const updateData = { lastNotificationTimestamp: currentTime };
+    await db.collection('users').doc(uid).set(updateData, { merge: true });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
+      }
+    });
+  });
+  
