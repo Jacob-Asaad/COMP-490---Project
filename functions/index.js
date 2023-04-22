e91cf6dee5d857de4e81eef4a1b59c845ffde132
valladaresassc <valladares.jose805@gmail.com>
2023-04-21 21:40:15 -0700

    Notifications work + Firebase functions started

--- functions/index.js
+++ functions/index.js
@@ -1,82 +1,38 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const { Expo } = require('expo-server-sdk');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-admin.initializeApp();
-const expo = new Expo({ accessToken: 'oxq6VuA_yrYliTDLeN0oCnwmyCammz09ct3ZG0TR' });
+admin.initializeApp(functions.config().firebase);
 
-exports.sendNotificationOnDatabaseUpdate = functions.database
-  .ref('/moistureSensor/')
-  .onUpdate(async (change, context) => {
-    const data = change.after.val();
+const db = admin.firestore();
 
-    // Get the push notification token for the user with the updated data
-    const userId = data.userId;
-    const userSnapshot = await admin.database().ref(`users/${userId}`).once('value');
-    const user = userSnapshot.val();
-    const pushToken = user.expoPushToken;
-
-    // Construct the Expo push notification message
-    const message = {
-      to: pushToken,
-      sound: 'default',
-      title: 'PlantLink',
-      body: 'Plant Data Updated',
-    };
-
-    // Send the Expo push notification
-    try {
-      const response = await expo.sendPushNotificationsAsync([message]);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
-  exports.sendDailyNotification = functions.pubsub.schedule('every 24 hours').onRun(async (context) => {
-    const usersSnapshot = await db.collection('users').get();
-  
-    // Iterate through each user
-    usersSnapshot.forEach(async (doc) => {
-      const uid = doc.id;
-      const userData = doc.data();
-  
-      // Check if user has given permission to receive notifications
-      if (userData.receiveNotifications) {
-  
-        // Check the last notification timestamp
-        const lastNotificationTimestamp = userData.lastNotificationTimestamp || 0;
-const currentTime = Date.now();
-
-if ((currentTime - lastNotificationTimestamp) >= (24 * 60 * 60 * 1000)) {
-  // It's been more than 24 hours since last notification
-
-  // Get the push notification token for the user
-  const pushTokenSnapshot = await db.collection('users').doc(uid).collection('pushTokens').doc('expoPushToken').get();
-  const pushToken = pushTokenSnapshot.data().token;
-
-  // Construct the Expo push notification message
-  const message = {
-    to: pushToken,
-    sound: 'default',
-    title: 'PlantLink',
-    body: "It's time to water your plants!",
-  };
-
-  // Send the Expo push notification
+exports.getUserData = functions.https.onRequest(async (req, res) => {
   try {
-    const response = await expo.sendPushNotificationsAsync([message]);
-    console.log(response);
-
-    // Update the user's last notification timestamp
-    const updateData = { lastNotificationTimestamp: currentTime };
-    await db.collection('users').doc(uid).set(updateData, { merge: true });
+    const data = await db.collection("users").get();
+    const users = [];
+    data.forEach((doc) => {
+      users.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+    return res.json(users);
   } catch (error) {
     console.error(error);
+    return res.status(500).json({error: "Something went wrong"});
   }
-}
+});
 
-      }
-    });
-  });
-  
+exports.addUserData = functions.https.onRequest(async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({error: "Method not allowed"});
+  }
+  try {
+    const {name, email} = req.body;
+    const user = {name, email};
+    const data = await db.collection("users").add(user);
+    return res.json({message: `User added with ID: ${data.id}`});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({error: "Something went wrong"});
+  }
+});
